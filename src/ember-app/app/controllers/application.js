c8b1d52ede196ce94c2b7712c628a9beba0fbea7
Flexberry-man <mail@flexberry.net>
2023-11-22 08:52:27 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/application.js
+++ src/ember-app/app/controllers/application.js
@@ -28,7 +28,6 @@ export default Controller.extend({
             link: 'i-i-s-9-регистр-l',
             caption: i18n.t('forms.application.sitemap.9.i-i-s-9-регистр-l.caption'),
             title: i18n.t('forms.application.sitemap.9.i-i-s-9-регистр-l.title'),
-            icon: 'building',
             children: null
           }, {
             link: 'i-i-s-9-сотрудник-l',
@@ -40,25 +39,25 @@ export default Controller.extend({
             link: 'i-i-s-9-заявка-l',
             caption: i18n.t('forms.application.sitemap.9.i-i-s-9-заявка-l.caption'),
             title: i18n.t('forms.application.sitemap.9.i-i-s-9-заявка-l.title'),
-            icon: 'folder',
+            icon: 'file',
             children: null
           }, {
             link: 'i-i-s-9-записи-l',
             caption: i18n.t('forms.application.sitemap.9.i-i-s-9-записи-l.caption'),
             title: i18n.t('forms.application.sitemap.9.i-i-s-9-записи-l.title'),
-            icon: 'table',
+            icon: 'paperclip',
             children: null
           }, {
             link: 'i-i-s-9-должности-l',
             caption: i18n.t('forms.application.sitemap.9.i-i-s-9-должности-l.caption'),
             title: i18n.t('forms.application.sitemap.9.i-i-s-9-должности-l.title'),
-            icon: 'edit',
+            icon: 'address card',
             children: null
           }, {
             link: 'i-i-s-9-контрагент-l',
             caption: i18n.t('forms.application.sitemap.9.i-i-s-9-контрагент-l.caption'),
             title: i18n.t('forms.application.sitemap.9.i-i-s-9-контрагент-l.title'),
-            icon: 'suitcase',
+            icon: 'paperclip',
             children: null
           }]
         }
